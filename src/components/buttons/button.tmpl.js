79b6023b956074be0e724dd49f4e03f222f8d927
Marukyan Albert <albertmarukyan@yandex.ru>
2022-10-12 21:09:35 +0300

    new commit

--- src/components/buttons/button.tmpl.js
+++
@@ -1,5 +0,0 @@
-export const button = `
-    <div class="{{wrapperClass}}">
-        <a href="{{link}}" class="{{innerClass}}">{{text}}</a>
-    </div>
-`;
--- src/pages/chats/chats.js
+++
@@ -1,7 +0,0 @@
-import { chats } from "./chats.tmpl";
-import Handlebars from "handlebars";
-import { chat } from "../../components/chats/chat";
-const template = Handlebars.compile(chats);
-const chatsBlock = template({});
-const root = document.querySelector("#root");
-root.insertAdjacentHTML("afterbegin", chatsBlock);
